af962451383d65523294ad8067565bcb61700412
hflippen <soa1@1020-14.mesa.gmu.edu>
2019-04-16 12:32:13 -0400

    project5v2

--- project5/js/number.js
+++ project5/js/number.js
@@ -1,15 +1,12 @@
  $(document).ready(function() {
-    var number = Math.floor(Math.random() * 1000001);
-  	console.log(number);
   	var guesses = '';
   	$('#submit').click(function() {
   	 	var guess = $('input').val();
       var one = 1;
-  	 	guesses += guess + ' ';
-  	 	$('.guesses p').text(guesses);
   		console.log(guess);
-      var correct = one+guess;
+      var correct = parseInt(guess, 10) + parseInt(one, 10);
       $('h1').text('Incorrect, you were actually thinking of ' + correct);
+      $('#container').toggleClass('visibility');
   		// if (number == guess) {
   		// 	$('h1').text(number + ' is right!');
   		// 	$('input').val('');
@@ -21,4 +18,8 @@
   		// 	$('input').val('');
   		// }
   	});
+    // $('#container').click(function() {
+    //   $('#submit').toggleClass('visibility');
+    // });
 });
+//$('input').val()
